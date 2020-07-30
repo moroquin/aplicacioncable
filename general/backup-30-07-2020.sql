1ea571962f342d325f2aa05eb808593d6ad66e29
Oliver Sierra <sierra.oe@gmail.com>
2020-07-30 09:58:59 -0600

    todo listo

--- general/backup-30-07-2020.sql
+++ general/backup-30-07-2020.sql
@@ -19,8 +19,6 @@
 -- Current Database: `controlServicios`
 --
 
-
-
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `controlServicios` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 
 USE `controlServicios`;
@@ -50,7 +48,7 @@ CREATE TABLE `clientes` (
   PRIMARY KEY (`idcliente`),
   KEY `fk_clientes_zona1_idx` (`nombrezona`),
   CONSTRAINT `fk_clientes_zona1` FOREIGN KEY (`nombrezona`) REFERENCES `zona` (`nombrezona`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -59,7 +57,7 @@ CREATE TABLE `clientes` (
 
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
-INSERT INTO `clientes` VALUES (1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0',NULL),(2,'1','Oliver ','Ernesto ','Sierra ','Pac','hola mundito mundito','1','adsf','234','234','1','barrio garibaldi',NULL),(3,'2','maria','rubby','morales','castillo','ciudad','2','hola mundo','555','55','2','Barrio cenizal',NULL);
+INSERT INTO `clientes` VALUES (1,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'0',NULL),(2,'1','Oliver ','e','Sierra ','p','hola mundito mundito','1','adsf asdf asdf asd','234234234','234','1','Barrio cenizal',''),(3,'2','maria','r','morales','c','ciudad','2','hola mundo','555','55','2','Barrio cenizal',''),(4,'3','3','3','3','3','333','3','3','3','3','3','barrio garibaldi',NULL),(5,'a','a','a','aa','a','a','a','','','','a','Barrio cenizal',NULL),(6,'32','Juan francisco ',NULL,'Rios López',NULL,'dirección ','2323','referencias','77562','','334','Barrio las flores','');
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +82,7 @@ CREATE TABLE `cobropormes` (
 
 LOCK TABLES `cobropormes` WRITE;
 /*!40000 ALTER TABLE `cobropormes` DISABLE KEYS */;
-INSERT INTO `cobropormes` VALUES ('2020-05',1,'now()');
+INSERT INTO `cobropormes` VALUES ('2020-6',1,'now()');
 /*!40000 ALTER TABLE `cobropormes` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -110,13 +108,21 @@ CREATE TABLE `cobros` (
   `mesespagados` int(11) DEFAULT NULL,
   `totalporcobrar` double DEFAULT NULL,
   `mesesporcobrar` int(11) DEFAULT NULL,
+  `lote_idlote` int(11) DEFAULT NULL,
+  `mesesporcobrardet` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `idcliente` int(11) NOT NULL,
+  `mesespagadosdet` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`idcobro`),
   KEY `fk_cobros_empleados1_idx` (`idempleado`) USING BTREE,
   KEY `fecha` (`fecha`) USING BTREE,
   KEY `fk_cobros_servicioscontratados1_idx` (`idservicioscontratados`),
+  KEY `fk_cobros_lote1` (`lote_idlote`),
+  KEY `fk_cobros_clientes1` (`idcliente`),
+  CONSTRAINT `fk_cobros_clientes1` FOREIGN KEY (`idcliente`) REFERENCES `clientes` (`idcliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_cobros_empleados1` FOREIGN KEY (`idempleado`) REFERENCES `empleados` (`idempleado`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cobros_lote1` FOREIGN KEY (`lote_idlote`) REFERENCES `lote` (`idlote`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_cobros_servicioscontratados1` FOREIGN KEY (`idservicioscontratados`) REFERENCES `servicioscontratados` (`idservicioscontratados`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,6 +131,7 @@ CREATE TABLE `cobros` (
 
 LOCK TABLES `cobros` WRITE;
 /*!40000 ALTER TABLE `cobros` DISABLE KEYS */;
+INSERT INTO `cobros` VALUES (99,NULL,1,NULL,0,14,NULL,'','','Barrio cenizal','2020-6',0,300,2,NULL,'Mayo, Junio',2,''),(100,NULL,1,NULL,0,14,NULL,'','','Barrio cenizal','2020-6',0,450,3,NULL,'Abril, Mayo, Junio',2,''),(101,NULL,1,NULL,0,15,NULL,'','','Barrio las flores','2020-6',0,900,3,NULL,'Abril, Mayo, Junio',6,''),(102,NULL,1,'2020-07-26',900,15,NULL,'12','12','Barrio las flores','2020-6',3,300,3,NULL,'Abril, Mayo, Junio',6,'Abril, Mayo, Junio');
 /*!40000 ALTER TABLE `cobros` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,7 +152,7 @@ CREATE TABLE `empleados` (
   PRIMARY KEY (`idempleado`),
   KEY `fk_empleados_puestos_idx` (`puestos_idpuestos`),
   CONSTRAINT `fk_empleados_puestos` FOREIGN KEY (`puestos_idpuestos`) REFERENCES `puesto` (`idpuestos`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -154,6 +161,7 @@ CREATE TABLE `empleados` (
 
 LOCK TABLES `empleados` WRITE;
 /*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (1,'empleado 1','empleado 1','telefono','direccion',1);
 /*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -178,8 +186,34 @@ CREATE TABLE `estado` (
 
 LOCK TABLES `estado` WRITE;
 /*!40000 ALTER TABLE `estado` DISABLE KEYS */;
-INSERT INTO `estado` VALUES ('Aprobado',' . . . ',0),('Finalizado',' . . . ',0),('Pendiente',' . . . ',10),('Por renovar',' . . . ',0);
+INSERT INTO `estado` VALUES ('Activo','Contratos activos ',0),('Finalizado','Contrato Finalizado',0),('Moroso','Contratos morosos ',0),('Suspendido','Contratos Suspendidos ',0);
 /*!40000 ALTER TABLE `estado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lote`
+--
+
+DROP TABLE IF EXISTS `lote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lote` (
+  `idlote` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` datetime DEFAULT current_timestamp(),
+  `totalcobrado` double DEFAULT NULL,
+  `anyomes` varchar(7) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `zona` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`idlote`)
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lote`
+--
+
+LOCK TABLES `lote` WRITE;
+/*!40000 ALTER TABLE `lote` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lote` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -195,7 +229,7 @@ CREATE TABLE `puesto` (
   `descripcion` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `nivel` int(11) DEFAULT NULL,
   PRIMARY KEY (`idpuestos`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -204,6 +238,7 @@ CREATE TABLE `puesto` (
 
 LOCK TABLES `puesto` WRITE;
 /*!40000 ALTER TABLE `puesto` DISABLE KEYS */;
+INSERT INTO `puesto` VALUES (1,'puesto 1','hola mundo ',10);
 /*!40000 ALTER TABLE `puesto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -255,6 +290,12 @@ CREATE TABLE `servicioscontratados` (
   `corte` varchar(4) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `nombreestado` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nombrezona` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `detmesesporpagar` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fechasuspension` date DEFAULT NULL,
+  `fechareconexion` date DEFAULT NULL,
+  `descripcionreconexion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `descripcionsuspension` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cobroreconexion` double DEFAULT NULL,
   PRIMARY KEY (`idservicioscontratados`),
   KEY `fk_servicioscontratados_clientes1_idx` (`idcliente`),
   KEY `fk_servicioscontratados_servicios1_idx` (`idservicio`),
@@ -262,7 +303,7 @@ CREATE TABLE `servicioscontratados` (
   CONSTRAINT `fk_servicioscontratados_clientes1` FOREIGN KEY (`idcliente`) REFERENCES `clientes` (`idcliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_servicioscontratados_estado1` FOREIGN KEY (`nombreestado`) REFERENCES `estado` (`nombre`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_servicioscontratados_servicios1` FOREIGN KEY (`idservicio`) REFERENCES `servicios` (`idservicio`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -271,14 +312,17 @@ CREATE TABLE `servicioscontratados` (
 
 LOCK TABLES `servicioscontratados` WRITE;
 /*!40000 ALTER TABLE `servicioscontratados` DISABLE KEYS */;
-INSERT INTO `servicioscontratados` VALUES (0,0,3,1,'2',NULL,18,'2020-05-18',2,'','Finalizado',NULL),(1,0,2,2,'4',300,4,'2020-05-27',4,NULL,'Aprobado',NULL);
+INSERT INTO `servicioscontratados` VALUES (3,0,2,1,'12',150,1,'2020-07-26',14,NULL,'Moroso','Barrio cenizal','Abril, Mayo, Junio','2020-07-26',NULL,'','suspendido por no pagar',NULL),(3,0,6,2,'12',300,2,'2020-07-26',15,NULL,'Suspendido','Barrio las flores','Abril, Mayo, Junio','2020-07-01','2020-07-26','Reconectado pago completo','suspendido por no pagar',580);
 /*!40000 ALTER TABLE `servicioscontratados` ENABLE KEYS */;
 UNLOCK TABLES;
 
-/var/www/html/aplicacioncable/general/backup.sql
+--
+-- Table structure for table `user`
+--
 
 DROP TABLE IF EXISTS `user`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -299,9 +343,15 @@ CREATE TABLE `user` (
   KEY `fk_user_empleados1_idx` (`empleados_idempleado`),
   CONSTRAINT `fk_user_empleados1` FOREIGN KEY (`empleados_idempleado`) REFERENCES `empleados` (`idempleado`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
 
 LOCK TABLES `user` WRITE;
-
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -323,7 +373,7 @@ CREATE TABLE `zona` (
 
 LOCK TABLES `zona` WRITE;
 /*!40000 ALTER TABLE `zona` DISABLE KEYS */;
-INSERT INTO `zona` VALUES ('0'),('Barrio cenizal'),('barrio garibaldi');
+INSERT INTO `zona` VALUES ('0'),('Barrio cenizal'),('barrio garibaldi'),('Barrio las flores');
 /*!40000 ALTER TABLE `zona` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -336,4 +386,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-27  9:44:15
+-- Dump completed on 2020-07-30  9:58:33
